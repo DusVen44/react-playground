923853e6ce0c9ae3c00d4a9aa62027d1f895b44a
Dustin Venable <Venny44@yahoo.com>
2020-05-29 11:40:16 -0500

    Accordion

---
+++ src/state-drills/Accordion.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react'
+
+
+export default class Accordion extends Component {
+    static defaultProps = {
+        sections: []
+    }
+
+    state = {
+        activeSectionIndex: null,
+    }
+
+    handleSetActiveSection = (sectionIndex) => {
+        this.setState({ activeSectionIndex: sectionIndex })
+    }
+
+    renderItem(section, index, activeSectionIndex) {
+        return (
+            <li className='Accordion-item' key={index}>
+                <button
+                  type='button'
+                  onClick={() => this.handleSetActiveSection(index)}
+                >
+                  {section.title}
+                </button>
+                {(activeSectionIndex === index) && <p>{section.content}</p>}
+            </li>
+        )
+    }
+
+    render() {
+        const { activeSectionIndex } = this.state
+        const { sections } = this.props
+        return (
+            <div>
+                <ul className='Accordion'>
+                  {sections.map((section, index) =>
+                   this.renderItem(section, index, activeSectionIndex)
+                   )}    
+                </ul> 
+            </div>
+        )
+    }
+}
